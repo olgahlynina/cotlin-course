2ea34bfcc3500a91a97626f15dfed5de0e80c974
olgakhlynina <olgahlynina@yandex.ru>
2026-04-12 12:56:04 +0300

    выполнено ДЗ по уроку Базовые типы данных

---
+++ src/main/kotlin/lessons/lesson04/homework/Datatypes.kt
@@ -0,0 +1,72 @@
+package ru.stimmax.lessons.lesson04.homework
+
+
+    val v1: Byte = 42
+    val v2: Long = 98765432123456789L
+    val v3: Float = 23.45f
+    val v4: Double = 0.123456789
+    val v5: String = "Kotlin & Java"
+    // присвоенное значение написано заглавными буквами
+    // val v6: Boolean = FALSE
+    val v7: Char = 'c'
+    val v8: Int = 500
+    val v9: Long = 4294967296L
+    val v10: Float = 18.0f
+    val v11: Double = -0.001
+    val v12: String = "OpenAI"
+    val v13: String = "true"
+    val v14: List<Byte> = listOf(3, 14)
+    val v15: Char = '9'
+    val v16: Int = 2048
+    val v17: Long = 10000000000L
+    val v18: Set<String> = setOf("OpenAI", "Quantum Computing")
+    val v19: Float = 5.75f
+    // присвоенное значение написано в кавычках
+    // val v20: Double = `1.414`
+    val v21: String = "Artificial Intelligence"
+    val v22: Array<Any> = arrayOf('x',"A")
+    val v23: String = "Android Studio"
+    val v24: Char = '@'
+    val v25: Int = 1024
+    val v26: Long = 1234567890123L
+    val v27: Float = 10.01f
+    val v28 = -273.15
+    val v29 = "SpaceX"
+    // присвоенное значение написано заглавными буквами
+    //val v30: Boolean = FALSE
+    val v31: Double = 0.007
+    // присвоенное значение написано в двойных кавычках
+    //val v32: Char = “🤯”
+    val v33: Map<String, Int> = mapOf("true" to 2, "false" to 34)
+    // присвоенное значение написано в кавычках
+    //val v34: Int = ‘65535’
+    val v35: Long = 72057594037927935L
+    val v36: Float = 2.71828f
+    val v37: Double = 101.0101
+    val v38: String = "Quantum Computing"
+    val v39: Map<Int, String> = mapOf(2 to "true", 34 to "false")
+    val v40: Char = 'x'
+    val v41: Int = 314
+    val v42: Long = 123456789123456789L
+    val v43: Float = 6.626f
+    // присвоенное значение написано заглавными буквами
+    //val v44: Boolean = TRUE
+
+    val s1: Char = 'a'
+    val s2: Double = 1.5
+    val s3: Set<Double> = setOf(785.50, 450.45)
+    val s4: Double = 0.2544554
+    val s5: Boolean = true
+    val s6: Int = 15
+    val s7: Byte = 0
+    val s8: Long = 334043222442211L
+    val s9: Double = 345.312
+    val s10: Map<String, Double> = mapOf("Василек" to 657.30)
+    val s11: String = "починилось само"
+    val s12: List<String> = listOf("тема1", "тема2")
+
+
+
+
+
+
